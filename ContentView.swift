5584f845577e2cdee6c72b826aa6280050be7431
Tommy Rokicki <rok512515@d204.lths.net>
2025-04-09 12:40:44 -0500

    Made textfield for number 2

--- ContentView.swift
+++ ContentView.swift
@@ -6,11 +6,7 @@ struct ContentView: View {
     @State var number2 = "number2"
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+        TextField("Enter a number",value: $number2, formatter: NumberFormatter())
     }
 }
