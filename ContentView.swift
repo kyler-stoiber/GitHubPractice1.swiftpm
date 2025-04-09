4397f5cb34f3281910ac1267d950cc8511414663
Kyler Stoiber <sto512144@d204.lths.net>
2025-04-09 12:40:12 -0500

    ok

--- ContentView.swift
+++ ContentView.swift
@@ -2,15 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var number = "number1"
-    @State var number2 = "number2"
+    @State var number = 1
+    @State var number2 = 2
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+    
+        Image(systemName: "globe")
+            .imageScale(.large)
+            .foregroundColor(.accentColor)
+        Text("Hello, world!")
+        
+        TextField("Enter a number: ", value: $number, formatter:  NumberFormatter())
     }
 }
