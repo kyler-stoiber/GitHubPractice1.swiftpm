b963d181ef6d2bb3162fb74b3e938754315a8113
Tommy Rokicki <rok512515@d204.lths.net>
2025-04-09 12:50:13 -0500

    BLah blah blah

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,7 @@ struct ContentView: View {
     
     @State var number = 1
     @State var number2 = 2
+    @State var answer = 0
     
     var body: some View {
     
@@ -14,5 +15,6 @@ struct ContentView: View {
         
         TextField("Enter a number",value: $number2, formatter: NumberFormatter())
         TextField("Enter a number: ", value: $number, formatter:  NumberFormatter())
+        Text("\(answer)")
     }
 }
